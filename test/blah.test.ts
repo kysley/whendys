77e6e98f2446c6672d3d764e18e25e2650da7fe6
Evan Kysley <evan@kysley.com>
2021-06-23 00:39:12 -0400

    fix linting

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -2,13 +2,13 @@ import { when } from '../src';
 
 describe('blah', () => {
   it('works', () => {
-    expect(when(new Date())).toEqual("Seconds ago");
+    expect(when(new Date())).toEqual('Seconds ago');
   });
   it('whens', () => {
-    expect(when(new Date('June 26, 2020'))).toEqual('11 months ago')
-  })
+    expect(when(new Date('June 26, 2020'))).toEqual('11 months ago');
+  });
 
-  it ('whens2', () => {
-    expect(when(new Date(1623643200000))).toEqual('1 week ago')
-  })
+  it('whens2', () => {
+    expect(when(new Date(1623643200000))).toEqual('1 week ago');
+  });
 });
